f7387250b2c23eeb1ec92f7d0b72c8b476826ad1
ulissesbcorrea <ulissesbcorrea@gmail.com>
2020-08-15 01:57:51 -0300

    correcting folder creation to p2

--- p2_multiple_runs.py
+++ p2_multiple_runs.py
@@ -5,7 +5,9 @@ import time
 start_time = time.time()
 SEEDS = [94196879,76466153,8269641,96259793,59870115,19470948,51319742,38792324,23867758,91518904]
 for seed in SEEDS:
-    os.makedirs(os.path.join('results',str(seed)),exist_ok = True)
+    p = os.path.join('results',str(seed))
+    if not os.path.exists
+        os.makedirs(p)
 for seed in SEEDS:
     print 'Trainning ATAE with seed ' + str(seed)
     start_it = time.time()
