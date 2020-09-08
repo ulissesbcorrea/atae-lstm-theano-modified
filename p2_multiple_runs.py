a6bedd9bdc4680a1d59ef1b691fc7f57e52e587d
ulissesbcorrea <ulissesbcorrea@gmail.com>
2020-09-08 16:18:30 -0300

    python 2 to 50 epochs

--- p2_multiple_runs.py
+++ p2_multiple_runs.py
@@ -3,6 +3,7 @@ import os
 import time
 start_time = time.time()
 SEEDS = [94196879,76466153,8269641,96259793,59870115,19470948,51319742,38792324,23867758,91518904]
+epochs = 50
 
 for seed in SEEDS:
     p = os.path.join('results',str(seed))
@@ -14,7 +15,7 @@ for seed in SEEDS:
     print 'Trainning ATAE with seed ' + str(seed)
     start_it = time.time()
     print 'python2  main.py --seed '+ str(seed)
-    os.system('python2  main.py --seed '+ str(seed))
+    os.system('python2  main.py --seed '+ str(seed) + ' --epochs ' + str(epochs))
     print 'Excuted with ' + str(seed) + ' in ' + str(time.time() - start_it) +  ' s'
 
 print 'executed all seeds in ' + str(time.time() - start_time) + ' s' 
